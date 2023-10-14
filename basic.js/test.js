f76c3bdaaa088674a6fab34e3977d5f465cc877a
Rajatsingh0702 <140223286+Rajatsingh0702@users.noreply.github.com>
2023-10-14 08:58:53 +0000

    modified repository

--- basic.js/test.js
+++ basic.js/test.js
@@ -1,1 +1,42 @@
 console.log("hello")
+//console.log("hello")
+//const myname="Rajat"
+//console.log(typeof(myname));
+
+//let myarray=["rajat", 2 , true, null , undefined]
+//console.log(myarray[3])
+
+// function square(num) {
+//     return num * num;
+// }
+// console.log(square(4));
+
+//const str = '      The quick brown fox jumps over the lazy dog.     ' ;
+
+// const words = str.split(' ');
+// console.log(words[3]);
+
+
+// const chars = str.split('');
+// console.log(chars[8]);
+
+
+// const strCopy = str.split();
+// console.log(strCopy);
+
+// console.log(str.trim())
+
+//console.log(Math.random())
+// const min=10
+// const max=20
+// console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+// const obj={
+//     name:"rajat",
+//     age:20,
+//     email:"rajat@gmail.com"
+// }
+// console.log(obj.age);
+// obj.email="rajat@google.com"
+// console.log(obj.email)
+
